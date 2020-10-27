63c7cf6c7b8663453171d8da6f5a556753ebe39f
mihailthebuilder <mihailmarian12@gmail.com>
2020-10-27 09:12:54 +0000

    hero section now responsive and fully functional

--- js/custom.js
+++ js/custom.js
@@ -8,4 +8,24 @@ $(() => {
     $("#dropdown-wrapper>img").toggleClass("flip");
 
   })
+
+  //validate postcode input when Start button clicked
+  $("#hero-section button").click(()=>{
+
+    let postcodeCheck = /^[A-Z]{1,2}\d{1,2}\s?\d{1,2}[A-Z]{1,2}$/.test($("#hero-section input").val());
+    
+    if (!postcodeCheck) {
+
+      //insert tooltip saying that format is incorrect
+      $("#hero-section p").css("opacity",100);
+
+    } else {
+
+      $("#hero-section p").css("opacity",0);
+
+      //go down and do the comparison
+    }
+
+  })
+
 });
