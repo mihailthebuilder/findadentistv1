f9e463ca96b1f15e64a2ed6d6e19d21fd12a0c72
mihailthebuilder <mihailmarian12@gmail.com>
2020-10-27 15:55:35 +0000

    table loading prep complete

--- js/custom.js
+++ js/custom.js
@@ -1,13 +1,3 @@
-//initialize dentist dataset
-let DENTISTS = [];
-
-Papa.parse("data.csv", {
-    download: true,
-    header: true,
-    step: function(row) {
-      DENTISTS.push(row.data);
-    }
-});
 
 //only activate when document has been loaded
 $(() => {
@@ -17,13 +7,15 @@ $(() => {
 
     $("#right-nav-wrapper").slideToggle();
     $("#dropdown-wrapper>img").toggleClass("flip");
-
+ 
   })
 
   //validate postcode input when Start button clicked
   $("#hero-section button").click(()=>{
 
-    let postcodeCheck = /^[A-Z]{1,2}\d{1,2}\s?\d{1,2}[A-Z]{1,2}$/.test($("#hero-section input").val());
+    let postcode = $("#hero-section input").val().toUpperCase();
+
+    let postcodeCheck = /^[A-Z]{1,2}\d{1,2}\s?\d{1,2}[A-Z]{1,2}$/.test(postcode);
     
     if (!postcodeCheck) {
 
@@ -35,9 +27,35 @@ $(() => {
       //ensure validation tooltip is hidden
       $("#hero-section p").css("opacity",0);
 
-      //go down and do the comparison
+      //initialize dentist dataset
+      let dentistDataset = [];
+
+      Papa.parse("data.csv", {
+          download: true,
+          header: true,
+
+          step: function(row) {
+            if (row.data["name"] != undefined) {
+              dentistDataset.push(row.data);
+            }
+          },
+
+          //go down and do the comparison
+          complete: loadTable(postcode.replace(/\s/g,''),dentistDataset) 
+      });
+
+
     }
 
   })
 
 });
+
+//load table with data and jQuery listeners
+
+function loadTable(inputPostcode,dentistArr) {
+
+  //create copy of DENTISTS where you can add distance and sort
+  console.log(inputPostcode,dentistArr);
+
+}
